266755b749d384029b7884659d17aa81d530ae61
Harsh sethi <hsethi5@myseneca.ca>
2023-01-24 03:34:35 -0500

    first website

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def ledmirror(request):
+    return render(request, 'ledmirror.html')
+
+def frame(request):
+    return render(request, 'frame.html')
